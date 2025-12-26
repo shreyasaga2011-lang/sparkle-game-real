ee820e3acc4d525afcabdd3f80e9a8d4c47d6ae3
Barooah <abibara@amazon.com>
2025-12-25 21:18:27 -0600

    d

--- sparkle-game-real/scripts/area_2d.gd
+++ sparkle-game-real/scripts/area_2d.gd
@@ -1,6 +1,4 @@
 extends Area2D
 
-
-func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"):
-		
+func _on_body_entered(body: Node) -> void:
+	get_tree().change_scene_to_file("res://scenes/level_2.tscn")
