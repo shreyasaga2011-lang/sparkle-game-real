03a80ae3d37e4786bc5756ca593176e75455a27e
Barooah <abibara@amazon.com>
2025-12-26 21:19:31 -0600

    stuffxtwo

--- sparkle-game-real/scenes/next_level_to_3.gd
+++ sparkle-game-real/scenes/next_level_to_3.gd
@@ -3,4 +3,4 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://scenes/level_2.tscn")
+	get_tree().change_scene_to_file("res://scenes/level_3.tscn")
